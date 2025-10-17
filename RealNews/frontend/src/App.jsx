14a0143609c5feefee8084d0823a6420a1ef2cf1
Ishmum <ishmumibrahim15@gmail.com>
2025-10-17 14:46:05 -0400

    web scraper

--- RealNews/frontend/src/App.jsx
+++ RealNews/frontend/src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <h1>Real News</h1>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div class="alert">
+        <h1>Amazon had a lawsuit!</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -26,4 +21,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
